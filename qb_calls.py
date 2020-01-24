e78d4bfad30dbf71e725b145a070ecaad811687c
Fred Major <sgtfredmajor@gmail.com>
2020-01-24 14:48:21 -0500

    Qb calls

---
+++ qb_calls.py
@@ -0,0 +1,1 @@
+calls_pass_QB =  [“ Quarterback audibles at the the line: blue 80 on three, blue 80… blue 80… hut hut hut.”, “Quarterback barks: Red 80! Red 80! Hut! Hut! Hut!”, “Quarterback: Green 18...Green 18….. set-hut!”, “Quarterback: play call “319…319… hut!” Quarterback: Ready…..  set hut!”]
