6ae34e75983b0a400d429f0c07873522337a22d0
Alan Jordan <alan.jordanbiz@gmail.com>
2020-01-23 19:02:17 -0500

    run play bug fixed

--- superbowl.py
+++ superbowl.py
@@ -52,7 +52,7 @@ while stick_position > 0:
 #(RUN PLAY) if the user inputs 2 (run) the statement will subtract the random number of yards gained from the stick position 
 #getting the user closer to the goal line.
     if user_input == 2:
-        stick_position -= abs(random_num_run)
+        stick_position -= random_num_run
         move_clock = random_num_run * 4
         print()
         print("User ran the football")
